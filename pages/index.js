d309ed8a64b19c57e83e12206b720d7fbfba0dc1
Franco Caballero <franco.caballero.v@gmail.com>
2022-02-22 23:12:39 -0300

    feat: se agregan sections de acerca de mi y tecnologias

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,31 @@
-import { Container, Flex, Heading, Image, Link } from '@chakra-ui/react'
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  Image,
+  Link,
+  Stack,
+  Text
+} from '@chakra-ui/react'
 import NextLink from 'next/link'
 import Layout from '../components/layouts/article'
+
+const Section = ({ title, children, ...props }) => {
+  return (
+    <Box p={10} boxShadow="xs" {...props}>
+      <Heading as="h2" fontSize={20}>
+        {title}
+      </Heading>
+      <Text pt={5}>{children}</Text>
+    </Box>
+  )
+}
 
 export default function Home() {
   return (
     <Layout>
-      <Container maxW={1000} pt={10}>
+      <Container maxW={1000} pt={{ base: 20, md: 10 }}>
         <Flex
           direction={{
             base: 'column',
@@ -22,17 +42,21 @@ export default function Home() {
             mr={10}
             rounded="full"
           />
-          <Flex direction="column">
-            <Heading as="h1" fontSize={50}>
+          <Flex direction="column" justify="center" pt={{ base: 5, md: 0 }}>
+            <Heading as="h1" fontSize={40}>
               Hola, Soy
             </Heading>
-            <Heading as="h1" fontSize={50}>
+            <Heading as="h1" fontSize={40}>
               Franco Caballero
             </Heading>
-            <Heading as="h2" fontSize={20}>
+            <Heading as="h2" fontSize={20} pt="10px">
               Desarrollador de Software
             </Heading>
-            <Flex mt={2}>
+            <Flex
+              mt={2}
+              pt="10px"
+              justify={{ base: 'center', md: 'flex-start' }}
+            >
               <NextLink href="https://github.com/FrancoCaballero" passHref>
                 <Link color="red" isExternal>
                   <Image
@@ -54,6 +78,28 @@ export default function Home() {
             </Flex>
           </Flex>
         </Flex>
+
+        <Section title="Acerca de mi" mt={{ base: '150px', md: 10 }}>
+          Soy un desarrollador de software con experiencia en back-end y
+          front-end, he trabajo con difentes lenguajes de programación, pero en
+          el que me estoy especializando es en Javascript.
+        </Section>
+        <Section mt={10} title="Tecnologías">
+          <Stack
+            direction={{ base: 'column', md: 'row' }}
+            align={{ base: 'center', md: 'flex-start' }}
+            wrap="wrap"
+            gap={5}
+          >
+            <Image src="/images/js.png" alt="Javascript" w="50px" />
+            <Image src="/images/node.png" alt="Javascript" w="50px" />
+            <Image src="/images/react.png" alt="Javascript" w="50px" />
+            <Image src="/images/next.png" alt="Javascript" w="50px" />
+            <Image src="/images/angular.png" alt="Javascript" w="50px" />
+            <Image src="/images/java.png" alt="Javascript" w="50px" />
+            <Image src="/images/postgresql.png" alt="Javascript" w="50px" />
+          </Stack>
+        </Section>
       </Container>
     </Layout>
   )
