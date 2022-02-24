4dd38e80add55051732cba108855f4612c7fe0e7
Franco Caballero <franco.caballero.v@gmail.com>
2022-02-24 17:56:06 -0300

    feat: se agrega array para tecnologias

---
+++ data/tecnologies.js
@@ -0,0 +1,52 @@
+export const tecnologies = [
+  {
+    src: '/images/html.png',
+    alt: 'HTML5'
+  },
+
+  {
+    src: '/images/css.png',
+    alt: 'CSS3'
+  },
+  {
+    src: '/images/js.png',
+    alt: 'JavaScript'
+  },
+  ,
+  {
+    src: '/images/node.png',
+    alt: 'Node.js'
+  },
+  {
+    src: '/images/react.png',
+    alt: 'React'
+  },
+  {
+    src: '/images/next.png',
+    alt: 'Next.js'
+  },
+  {
+    src: '/images/angular.png',
+    alt: 'Angular'
+  },
+  {
+    src: '/images/java.png',
+    alt: 'Java'
+  },
+  {
+    src: '/images/postgresql.png',
+    alt: 'PostgreSQL'
+  },
+  {
+    src: '/images/chakra.png',
+    alt: 'Chakra UI'
+  },
+  {
+    src: '/images/bootstrap.png',
+    alt: 'Bootstrap'
+  },
+  {
+    src: '/images/docker.png',
+    alt: 'Docker'
+  }
+]
