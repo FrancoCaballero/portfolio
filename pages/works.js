6013d112f8ec0ecd9fbb7b2e98fd754ffca37c5a
Franco Caballero <franco.caballero.v@gmail.com>
2022-02-26 18:51:50 -0300

    feat: se crea pagina de trabajos

--- pages/works.js
+++ pages/works.js
@@ -1,21 +1,47 @@
-import { Container, Flex } from '@chakra-ui/react'
+import { Code, Container, Flex, Link, Text } from '@chakra-ui/react'
+import Image from 'next/image'
+import NextLink from 'next/link'
 import React from 'react'
 import Layout from '../components/layouts/article'
+import Section from '../components/layouts/section'
+
+const WorkItem = ({ children, title, href, src, alt }) => {
+  return (
+    <Section title={title}>
+      <Flex direction="column" justify="center" align="center">
+        <NextLink href={href} passHref>
+          <Link isExternal>
+            <Image src={src} width={200} height={350} alt={alt} />
+          </Link>
+        </NextLink>
+        <Code mt={3}>{children}</Code>
+      </Flex>
+    </Section>
+  )
+}
 
 const works = () => {
   return (
     <Layout>
-      <Container maxW={1000} pt={{ base: 20, md: 10 }}>
-        <Flex
-          direction={{
-            base: 'column',
-            md: 'row'
-          }}
-          height={300}
-          align="center"
-          justify="center"
-        >
-          <h1>Works</h1>
+      <Container maxW={1000} pt={20}>
+        <Flex direction="column" align="center" justify="center" gap={10}>
+          <WorkItem
+            title="Gif Expert"
+            href="https://gif-expert-rosy.vercel.app/"
+            src="/images/gif-expert.jpg"
+            alt="Aplicación de gifs"
+          >
+            Aplicación de busqueda de gifs, hecho con Reacj.js
+          </WorkItem>
+
+          <WorkItem
+            title="Search Movies"
+            href="https://search-movies-six.vercel.app/"
+            src="/images/search-movies.jpg"
+            alt="Aplicación de películas"
+          >
+            Aplicación de busqueda de películas, hecho con Svelte.js
+          </WorkItem>
         </Flex>
       </Container>
     </Layout>
