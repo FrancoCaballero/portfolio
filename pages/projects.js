ee53439428660a914c71145aca5ab3ac3765300d
Franco Caballero <franco.caballero@bennu.cl>
2022-03-07 15:07:53 -0300

    feat: se cambia de nombre works a projects

--- pages/projects.js
+++ pages/projects.js
@@ -5,47 +5,48 @@ import React from 'react'
 import Layout from '../components/layouts/article'
 import Section from '../components/layouts/section'
 
-const WorkItem = ({ children, title, href, src, alt }) => {
+const ProjectItem = ({ children, title, href, src, alt }) => {
   return (
-    <Section title={title}>
-      <Flex direction="column" justify="center" align="center">
-        <NextLink href={href} passHref>
-          <Link isExternal>
+    <NextLink href={href} passHref>
+      <Link isExternal w="100%">
+        <Section title={title}>
+          <Flex direction="column" justify="center" align="center">
             <Image src={src} width={200} height={350} alt={alt} />
-          </Link>
-        </NextLink>
-        <Code mt={3}>{children}</Code>
-      </Flex>
-    </Section>
+
+            <Code mt={3}>{children}</Code>
+          </Flex>
+        </Section>
+      </Link>
+    </NextLink>
   )
 }
 
-const works = () => {
+const projects = () => {
   return (
     <Layout>
       <Container maxW={1000} pt={20}>
         <Flex direction="column" align="center" justify="center" gap={10}>
-          <WorkItem
+          <ProjectItem
             title="Gif Expert"
             href="https://gif-expert-rosy.vercel.app/"
             src="/images/gif-expert.jpg"
             alt="Aplicación de gifs"
           >
             Aplicación de busqueda de gifs, hecho con Reacj.js
-          </WorkItem>
+          </ProjectItem>
 
-          <WorkItem
+          <ProjectItem
             title="Search Movies"
             href="https://search-movies-six.vercel.app/"
             src="/images/search-movies.jpg"
             alt="Aplicación de películas"
           >
             Aplicación de busqueda de películas, hecho con Svelte.js
-          </WorkItem>
+          </ProjectItem>
         </Flex>
       </Container>
     </Layout>
   )
 }
 
-export default works
+export default projects
